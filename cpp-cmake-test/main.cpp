c8ba6e7f1bdc09907a5c02ccda60962c4815fb78
kcodek <career_kk@yahoo.com>
2018-05-17 14:41:55 +0530

    using Boost: no compiling

--- cpp-cmake-test/main.cpp
+++ cpp-cmake-test/main.cpp
@@ -3,9 +3,18 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
+#include<string>
+using std::string;
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 #include "ToDoCore/ToDo.h"
+// using ToDoCore::ToDo;
 
 #define EXPECT_EQUAL(test,expect) equalityTest(test, expect, #test, #expect, __FILE__, __LINE__)
+
+
 
 template <typename T1, typename T2>
 int equalityTest(const T1 testValue,
@@ -15,26 +24,79 @@ int equalityTest(const T1 testValue,
                  const char* fileName,
                  const int lineNumber);
 
-int main( int argc, char** argv){
-    int result =0;
+int main( int argc, char** argv)
+{
+    po::options_description desc("Options");
+    desc.add_options()
+        ("help.h", "display this help")
+        ("add,a",po::value< string >(),"add a new entry to the To Do list" )
+        ;
+    bool parseError = false;
+    po::variables_map vm;
+    try{
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch (po::error& error)
+    {
+        cerr << "Error: " << error.what() << "\n" << endl;
+        parseError = true;
+    }
+    if (parseError || vm.count("help"))
+    {
+        cout << "todo:  A simple To Do list program" << "\n";
+        cout                                         << "\n";
+        cout << "Usage:"                             << "\n";
+        cout << "  " << argv[0] << " [options]"      << "\n";
+        cout                                         << "\n";
+        cout << desc                                 << "\n";
+
+        if (parseError)
+        {
+            return 64;
+        }
+        else
+        {
+            return 0;
+        }
+    }
     ToDo list;
 
     list.addTask("write code");
     list.addTask("compile");
     list.addTask("test");
 
-    result |= EXPECT_EQUAL(list.size(), size_t(3));
-    result |= EXPECT_EQUAL(list.getTask(0),"write code");
-    result |= EXPECT_EQUAL(list.getTask(1),"compile");
-    result |= EXPECT_EQUAL(list.getTask(2),"test");
-
-    if(result == 0)
+    if (vm.count("add"))
     {
-        cout<< "Test Passed"<<endl;
+        list.addTask(vm["add"].as< string >());
     }
 
+    for (size_t i = 0; i < list.size(); ++i)
+    {
+        cout << list.getTask(i) << "\n";
+    }
+    return 0;
 
-    return result;
+
+
+
+    // int result =0;
+    // ToDo list;
+
+    // list.addTask("write code");
+    // list.addTask("compile");
+    // list.addTask("test");
+
+    // result |= EXPECT_EQUAL(list.size(), size_t(3));
+    // result |= EXPECT_EQUAL(list.getTask(0),"write code");
+    // result |= EXPECT_EQUAL(list.getTask(1),"compile");
+    // result |= EXPECT_EQUAL(list.getTask(2),"test");
+
+    // if(result == 0)
+    // {
+    //     cout<< "Test Passed"<<endl;
+    // }
+    //return result;
 }
 
 template <typename T1, typename T2>
