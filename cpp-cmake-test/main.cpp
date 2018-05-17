5715fe7b2ad2854e156d3f6668f9977f0047ca80
kcodek <career_kk@yahoo.com>
2018-05-17 10:59:50 +0530

    Libraries and Subdirectories

--- cpp-cmake-test/main.cpp
+++ cpp-cmake-test/main.cpp
@@ -3,7 +3,7 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
-#include "ToDo.h"
+#include "ToDoCore/ToDo.h"
 
 #define EXPECT_EQUAL(test,expect) equalityTest(test, expect, #test, #expect, __FILE__, __LINE__)
 
